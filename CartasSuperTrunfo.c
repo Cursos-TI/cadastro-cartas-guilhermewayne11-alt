08c686d9632f64beeccabf980ca4289216ab0d5c
guilhermewayne11-alt <guilherme.wayne11@gmail.com>
2025-09-24 21:41:00 +0000

    Criei um programa de desafio da estacio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,88 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado1;
+    char codigo1[4];
+    char nomeCidade1[100];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+    
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[100];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
 
-  // Área para entrada de dados
+    printf("Desafio Super Trunfo!\n");
 
-  // Área para exibição dos dados da cidade
+    printf("Cadastro da carta 1:\n");
+    printf("Informe o estado (letra de A a H): ");
+    scanf("%c", &estado1);
+
+    printf("Informe o codigo da carta(ex:A01): ");
+    scanf("%s", codigo1);
+
+    printf("informe o nome da cidade: \n");
+    scanf("%s[^\n]", nomeCidade1);
+
+    printf("Informe a população da cidade: ");
+    scanf("%d", &populacao1);
+
+    printf("Informe a área da cidade (em km²): ");
+    scanf("%f", &area1);
+
+    printf("Informe o PIB da cidade (em bilhões de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Informe o número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
+
+    printf("\nCadastro da Carta 2:\n");
+    printf("Informe o estado (letra de A a H): ");
+    scanf(" %c", &estado2);
+
+    printf("Informe o código da carta (ex: B02): ");
+    scanf("%s", codigo2);
+
+    printf("Informe o nome da cidade: ");
+    scanf(" %s[^\n]", nomeCidade2);
+
+    printf("Informe a população da cidade: ");
+    scanf("%d", &populacao2);
+
+    printf("Informe a área da cidade (em km²): ");
+    scanf("%f", &area2);
+
+    printf("Informe o PIB da cidade (em bilhões de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Informe o número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos2);
+
+    // Exibição dos dados da Carta 1
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+
+    // Exibição dos dados da Carta 2
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
 return 0;
 } 
